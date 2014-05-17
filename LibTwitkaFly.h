9154ad3b2fbace500e32f7420f2cc47170a22856
moeseth <slamduck@gmail.com>
2014-05-18 00:07:28 +1000

    Update LibTwitkaFly.h

--- LibTwitkaFly.h
+++ LibTwitkaFly.h
@@ -6,7 +6,13 @@
 
 + (id) sharedTwitkaFly;
 - (BOOL) showSheetWithInitialText:(NSString *)text andInitialImage:(UIImage *)image;
+
+// deprecated in the iOS7 version
 - (void) QRWithText:(NSString *)text fromUser:(NSString *)sender forPostID:(NSString *)tweetID toOwner:(NSString *)ownerName;   // required the latest version of twitkafly
+
+// use this method for iOS7 version
+- (BOOL) showQRForBulletin:(BBBulletin *)bulletin;
+
 - (BOOL) hideSheet;
 
 @end
